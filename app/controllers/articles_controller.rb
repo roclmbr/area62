6d42034b9cd6c9f51185364e51dc4f4ef6683d43
Steve Adamson <gitlab@steveadamson.com>
2016-11-26 20:07:34 -0400

    Formatted error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
    def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article successfully created"
+            flash[:success] = "Article successfully created"
             redirect_to article_path(@article)   
         else
             render 'new'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
-            flash[:notice] = "Article successfully updated"
+            flash[:success] = "Article successfully updated"
             redirect_to article_path(@article)   
         else
             render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
     def destroy
        @article = Article.find(params[:id])
        @article.destroy
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
         redirect_to articles_path
     end
     
