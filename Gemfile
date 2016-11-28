3e84ce689e5e750c0d5010a18af794ff129e5112
Steve Adamson <gitlab@steveadamson.com>
2016-11-27 21:55:03 -0400

    validate users

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'bcrypt', '~> 3.1.7'
 gem 'sqlite3'
 gem 'puma', '~> 3.0'
 gem 'bootstrap-sass', '~> 3.3.5'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_many :articles
     before_save { self.email = email.downcase }
+    has_secure_password
     validates :username, presence:true, 
             uniqueness:{ case_sensitive: false }, 
             length:{minimum: 3, maximum: 25 }
@@ -10,6 +11,9 @@ class User < ActiveRecord::Base
     validates :email, presence:true, length:{ maximum: 105 },
             uniqueness:{ case_sensitive: false },
             format:{ with: VALID_EMAIL_REGEX }
+   
     
     validates :comment, presence: true
+    
+    
 end
