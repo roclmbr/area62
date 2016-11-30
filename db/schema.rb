e3ba20fe9c3ed56094d6fd779b15878045f425a8
Steve Adamson <gitlab@steveadamson.com>
2016-11-29 21:37:05 -0400

    Finished admin log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128012131) do
+ActiveRecord::Schema.define(version: 20161129233931) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20161128012131) do
     t.string   "username"
     t.string   "email"
     t.string   "comment"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
